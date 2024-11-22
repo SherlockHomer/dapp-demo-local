7f3d094fce77a0a10db907bb87b47d23fb37200b
shaowei.zhou <shaowei.zhou@okg.com>
2024-11-22 18:38:51 +0800

    update pkg name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import VConsole from 'vconsole';
 import ReactDOM from 'react-dom/client';
-import OKXConnectSDK from 'dapp-connect-sdk';
+import OKXConnectSDK from 'dapp-connect';
 import App from './App';
 
 import './index.css';
